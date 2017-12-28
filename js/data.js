89cde09b31e1e8f8c79566e4599550fc5aa179e4
MarinaRH <l_marina1717@hotmail.com>
2017-12-28 18:12:14 -0500

    agregando data de restaurant

---
+++ js/data.js
@@ -0,0 +1,34 @@
+var data = [
+  {
+    'typeFood':'coreana',
+    'name':'Sr Kim',
+    'addres':'san Borja 333',
+    'phone':'374777',
+    'class':'gourmet',
+    'img':'../assets/img/coreano.jpg'
+  },
+  {
+    'typeFood':'peruvian',
+    'name':'Sr uuuu',
+    'addres':'san Borja 333',
+    'phone':'374777',
+    'class':'gourmet',
+    'img':'../assets/img/comida2.jpg'
+  },
+  {
+    'typeFood':'coreana',
+    'name':'jjfjfjfjf Kim',
+    'addres':'san Borja 333',
+    'phone':'374777',
+    'class':'gourmet',
+    'img':'../assets/img/coreano2.jpg'
+  },
+  {
+    'typeFood':'peruvian',
+    'name':'aaaaa',
+    'addres':'san Borja 333',
+    'phone':'374777',
+    'class':'gourmet',
+    'img':'../assets/img/comida3.jpg'
+  }
+]

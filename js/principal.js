839a3593ee3385d711963f19b13eb033bb6d2fe5
MarinaRH <l_marina1717@hotmail.com>
2017-12-29 00:19:01 -0500

    modificando informacion de modal

--- js/principal.js
+++ js/principal.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
   var $img=$('#img-foot');
+  var modal = $('.modal');
 
   // funcion para mostrar imagenes de restaurantes
   function showImage(event) {
     for (i = 0; i < data.length; i++) {
-      $img.append('<div class="collection"><p class="name" id="type">' + data[i].typeFood + '</p><a href="#"   data-toggle="modal" data-target="#myModal"><img class="img"  src=' + data[i].img + '></a></div>');
+      $img.append('<div class="collection" data-name="' + data[i].name + '" data-toggle="modal" data-target="#myModal"><p class="typ">' + data[i].typeFood + '</p><a href="#" ><img class="img"  src=' + data[i].img + '></a></div>');
     }
   }
   showImage();
@@ -31,7 +32,11 @@ $(document).ready(function(){
   // funcion para agregar datos a modal
   $('.collection').on('click',function() {
     for (i = 0; i < data.length; i++) {
-      $('.modal-body').append('<div class="text-center"><h1>' + data[i].name + '</h1><div><img class="img"  src=' + data[i].img + '></div><span ></span>' + data[i].addres + '<span>' + data[i].phone + '</span><button type="button" class="btn btn-primary">Pedido</button></div>');  
+      if ($(this).data('name') === data[i].name) {
+        // $('.modal-body').append('<div class="text-center"><h1>' + data[i].name + '</h1><div><img  src=' + data[i].img + '></div><span ></span>' + data[i].addres + '<span>' + data[i].phone + '</span><button type="button" class="btn btn-primary">Pedido</button></div>'); 
+        $('#img-modal').attr('src', data[i].img);
+        $('#title-modal').text(data[i].addres);
+      }
     }
   });
 });

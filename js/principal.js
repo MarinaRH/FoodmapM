0b659445e0ba265405ce291cb49aa28d098b4282
MarinaRH <l_marina1717@hotmail.com>
2017-12-28 19:54:48 -0500

    Agregando modal a los restaurant

--- js/principal.js
+++ js/principal.js
@@ -1,21 +1,19 @@
 $(document).ready(function(){
   var $img=$('#img-foot');
+
   // funcion para mostrar imagenes de restaurantes
   function showImage(event) {
     for (i = 0; i < data.length; i++) {
-      $img.append('<div class="collection"><p class="name" id="type">' + data[i].typeFood + '</p><img class="img"  src=' + data[i].img + '></div>');
+      $img.append('<div class="collection"><p class="name" id="type">' + data[i].typeFood + '</p><a href="#"   data-toggle="modal" data-target="#myModal"><img class="img"  src=' + data[i].img + '></a></div>');
     }
   }
   showImage();
-   
-
-
-  // funcion para filtrar por tipo de comida
   
+  // primero ocultar todas las imagenes de restaurant
   $('#search').keyup(function() {
     var nombre = $(this).val();
     $('.collection').hide();
-
+    // funcion para filtrar por tipo de comida
     $('.collection').each(function() {
       var search = $(this).text();
       if (search.indexOf(nombre) !== -1) {

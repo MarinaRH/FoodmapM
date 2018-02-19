ac4224e2f42e27113891a39dafa7d50ebaab6a5d
MarinaRH <l_marina1717@hotmail.com>
2018-02-19 10:21:54 -0500

    Agregando firebase

--- js/app.js
+++ js/app.js
@@ -3,4 +3,15 @@ $(document).ready(function(){
   setTimeout(function(){
     window.location.href='views/principal.html';
   },3000);
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCui1rQAktiS8QCrER-YX-Y_m1P0QHXPoU",
+    authDomain: "foodmap-7f8d1.firebaseapp.com",
+    databaseURL: "https://foodmap-7f8d1.firebaseio.com",
+    projectId: "foodmap-7f8d1",
+    storageBucket: "foodmap-7f8d1.appspot.com",
+    messagingSenderId: "872073817745"
+  };
+  firebase.initializeApp(config);
 });
